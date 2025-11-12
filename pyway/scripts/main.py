ccd6d7d4309ca1546ae65c66be06ce32e482324b
femabeh <femabeh@gmail.com>
2025-11-12 12:45:01 +0100

    updated command.

--- pyway/scripts/main.py
+++ pyway/scripts/main.py
@@ -13,10 +13,28 @@ def cli():
 
 
 @cli.command()
+@click.option('--db_ms', '-m', help='What database type to use. Options: postgres, oracle, mysql, sqlserver.')
+@click.option('--host', '-h', help='Host to connect to.')
+@click.option('--schema', '-s', help='Database schema to use.')
+@click.option('--username', '-u', help='User to use when connecting.')
+@click.option('--password', '-p', help='Users password.')
+def migrate(db_ms: str = None, host: str = None, schema: str = None, username: str = None, password: str = None):
+    logger.info('STARTING MIGRATE PROCESS . . .')
+    if db_ms: settings.DBMS = db_ms
+    if host: settings.DATABASE_URL = host
+    if schema: settings.DATABASE_NAME = schema
+    if username: settings.DATABASE_USERNAME = username
+    if password: settings.DATABASE_PASSWORD = password
+    Migrate(settings).run()
+    logger.info('.MIGRATE ENDED.')
+
+
+"""@cli.command()
 def migrate():
     logger.info('STARTING MIGRATE PROCESS . . .')
     Migrate(settings).run()
     logger.info('.MIGRATE ENDED.')
+"""
 
 
 @cli.command()
